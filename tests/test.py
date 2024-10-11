3287045d5856aac43e99f39cea9876916c3a0d07
jahirduarte <jahir.duarte.inf@gmail.com>
2024-10-10 18:47:58 -0600

    Initial repository

---
+++ tests/test.py
@@ -0,0 +1,22 @@
+def calculate_total(products):
+    total = 0
+    for product in products:
+        # total *= product["price"]
+        total += product["price"]
+    return total
+
+def test_calculate_total_with_empty_list():
+    print("prueba")
+    assert calculate_total([]) == 0
+
+def test_calculate_total_with_single_product():
+    products = [
+        {
+            "name": "Notebook", "price": 5
+        }
+    ]
+    assert calculate_total(products) == 5
+
+if __name__ == "__main__":
+    test_calculate_total_with_empty_list()
+    test_calculate_total_with_single_product()
